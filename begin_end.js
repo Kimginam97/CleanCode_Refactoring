7b500308a216cf5b6669767c2b5fdd75f787a4e9
Kimginam97 <rlarlska79@naver.com>
2022-06-06 15:45:06 +0900

    경계를 포함하지만 제외하는 경우

---
+++ begin_end.js
@@ -0,0 +1,13 @@
+/* 
+    경계를 포함하지만 제외하는 경우
+    시작은 동일하지만 끝이 다른경우 
+    체크인 - begin
+    체크아웃 - end
+*/
+
+function reservationDate(beginDate, endDate){
+    // ...some code
+
+}
+
+reservationDate('YYYY-MM-DD','YYYY-MM-DD');
