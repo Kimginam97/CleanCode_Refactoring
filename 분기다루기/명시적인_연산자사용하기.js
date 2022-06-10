1c136e890c3018a13a302de12771e5bd942e3be7
Kimginam97 <rlarlska79@naver.com>
2022-06-10 09:16:17 +0900

    명시적인 연사자 사용하기

---
+++ 분기다루기/명시적인_연산자사용하기.js
@@ -0,0 +1,16 @@
+let number;
+
+function increment(number) {
+    number--;
+    --number;
+}
+
+function increment(number) {
+    ++number;
+    number++;
+}
+
+(몸무게 / (신장 * 신장))
+
+// 가로를 사용해서 우선순위 지정하기
+// if((isLogin && token) || user)
